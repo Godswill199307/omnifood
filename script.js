4b399b855d9f7a1c7b17f3e76968ed37404d5cd3
promisegabriella <gabrielgodswill2018@gmail.com>
2021-02-10 16:48:44 -0800

    omnifood

--- script.js
+++ script.js
@@ -1,138 +1,43 @@
-/********************************
- * variable and data types
- */
-/*
-var firstName = 'John';
-console.log(firstName);
+$(document).ready(function() {
 
-var lastName = 'Smith';
-var age = 28;
-
-var fullAge = true;
-console.log(fullAge);
-
-var job;
-console.log(job);
-
-job = 'Teacher';
-console.log(job);
-
-// variable naming rules
-var _3years = 3;
-var johnMark = 'John and Mark';
-// var if = 23; 
-*/
-
-
-/********************************
- * variable mutation and type coercion
- */
-
-/*
- var firstName = 'John';
- var age = 28;
-
- // Type coercion
- console.log(firstName + ' ' +  age);
-
- var job, isMarried;
- job = 'teacher';
- isMarried = false;
-
- console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
- // variable mutation
- age = 'twenty eight';
- job = 'driver';
-
- alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
- var lastName = prompt('what is his last Name?');
- console.log(firstName + ' ' + lastName); 
- */
+    /* for the sticky navigation */
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'   
+    }); 
 
 
 
- /********************************
-  * Basic operators
-  */
- /*
- var year, yearJohn, yearMark;
- now = 2018;
- ageJohn = 28;
- ageMark = 33;
+    /* scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
 
- // Math operators
- yearJohn = now - ageJohn;
- yearMark = now - ageMark;
- 
- console.log(yearJohn);
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+    
 
- console.log(now + 2);
- console.log(now * 2);
- console.log(now / 10);
+    /* Navigation scroll */
 
- 
-// Logical operators
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
 
-
-// type of operator
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof 'Mark is older than John');
-var x;
-console.log(typeof x);
-*/
-
-
-
-/*************************
- * operator procedence
- */
-
- /*
- var now = 2018;
- var yearJohn = 1989;
- var fullAge = 18;
-
- // Multiple operators
- var isFullAge = now - yearJohn >= fullAge; // true
- console.log(isFullAge);
-
- // Grouping
- var ageJohn = now - yearJohn;
- var ageMark = 35;
- var average = (ageJohn + ageMark) / 2;
- console.log(average);
-
-// Multiple assignments
-var x, y;
-x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
-console.log(x, y);
-
-
-// more operators
-x *= 2;
-console.log(x);
-x += 10;
-console.log(x);
-x--;
-console.log(x);
-*/
-
-
-
-var massMark = 78; // kg
-var heightMark = 1.69; // meters
-
-var massJohn = 92;
-var heightJohn = 1.95;
-
-var BMIMark = massMark / (heightMark * heightMark);
-var BMIJohn = massJohn / (heightJohn * heightJohn);
-
-var markHigherBMI = BMIMark > BMIJohn;
-console.log('Is Mark\'s BMI higher than John\'s? ');
-
+});
